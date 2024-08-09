4e599c48051fe123fb763930b99e4e7cc0e2494e
Brahim TAHIRI <pro.tahiri.brahim@gmail.com>
2024-08-09 17:02:27 +0200

    Initialisation de l'application

---
+++ src/main/java/fr/brahimtahiri/edunoteserver/EduNoteServerApplication.java
@@ -0,0 +1,13 @@
+package fr.brahimtahiri.edunoteserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduNoteServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduNoteServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/brahimtahiri/edunoteserver/EduNoteServerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.brahimtahiri.edunoteserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduNoteServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
